d80a856bb126fba229b8e5f9d6d3565bcc7ca38a
Attiksh Ansool Panda <aap184@case.edu>
2025-04-16 14:00:28 -0400

    changed local host settings

--- WaitlistWizard/server/index.ts
+++ WaitlistWizard/server/index.ts
@@ -56,15 +56,9 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 5000
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
-  const port = 5006;
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
-  });
-})();
+    // Start the server on port 5001
+    const port = 5001;
+    server.listen({ port, host: "0.0.0.0" }, () => {
+      log(`serving on port ${port}`);
+    });
+  })();
