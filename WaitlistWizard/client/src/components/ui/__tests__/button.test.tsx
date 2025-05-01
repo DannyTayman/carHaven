856ebc4b7d53004ae5c058b9794089f1504f87b3
Attiksh Ansool Panda <aap184@case.edu>
2025-04-30 22:54:14 -0400

    testing

--- WaitlistWizard/client/src/components/ui/__tests__/button.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { Button } from '../button';
-
-test('renders button with text', () => {
-  render(<Button>Click Me</Button>);
-  expect(screen.getByText(/click me/i)).toBeInTheDocument();
-});
